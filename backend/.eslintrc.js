ac83691030280d0dfc1b73969b44c9f03f28c5bf
向洋 <longdaluoshen@gmail.com>
2025-09-19 16:12:22 +0800

    Update .eslintrc.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+        project: ['./tsconfig.json']
     },
     plugins: ['@typescript-eslint'],
     rules: {
