08af324fe535e9060c37db597bd0a6e92cd7735b
向洋 <longdaluoshen@gmail.com>
2025-09-19 16:39:29 +0800

    Update .eslintrc.cjs

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:react-hooks/recommended'
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
